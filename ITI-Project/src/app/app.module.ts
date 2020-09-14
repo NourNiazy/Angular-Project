2d0d48f34f8259b53dc044b3150f09a2b181585f
ashrqat <71236765+Ashrqat19@users.noreply.github.com>
2020-09-14 02:24:46 +0200

    OrginS7r

--- ITI-Project/src/app/app.module.ts
+++ ITI-Project/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TestOrginComponent } from './test-orgin/test-orgin.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TestOrginComponent
   ],
   imports: [
     BrowserModule,
