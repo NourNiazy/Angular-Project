9dde737ed2217d13c8b9d7936f39ad6c78c87c79
ElshaymaaAhmed <61579659+ElshaymaaAhmed@users.noreply.github.com>
2020-09-18 23:14:55 +0200

    new change

--- Team-Angular-Project/src/app/app.module.ts
+++ Team-Angular-Project/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TestOrginComponent } from './test-orgin/test-orgin.component';
+import { HomeComponentComponent } from './home-component/home-component.component';
 import { CartComponentComponent } from './cart-component/cart-component.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TestOrginComponent,
+    HomeComponentComponent,
     CartComponentComponent
   ],
   imports: [
