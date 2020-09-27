688398c7aa29b956bbfa44b9b7c9c0bd18c54ff9
ElshaymaaAhmed <61579659+ElshaymaaAhmed@users.noreply.github.com>
2020-09-27 13:59:11 +0200

    product branch

--- Angular-Project/src/app/Interfaces/IProduct.ts
+++ Angular-Project/src/app/Interfaces/IProduct.ts
@@ -3,7 +3,9 @@ export interface IProduct
     id:number,
     img_url:string, 
     price:string,
+    category:string,
     rate:number,
+    rateList:[],
     title:string,
     txt:string 
 }
--- Angular-Project/src/app/product/product.component.ts
+++ Angular-Project/src/app/product/product.component.ts
@@ -10,11 +10,48 @@ export class ProductComponent implements OnInit {
 
   public Products=[];
 
+  category_0:boolean=false;
+  category_1:boolean=false;
+  category_2:boolean=false; 
+  category_3:boolean=false;
+
   constructor(private GetAllProducts:ProductService) { }
 
   ngOnInit(): void {
 
     this.GetAllProducts.getAllProducts().subscribe(data=>this.Products=data);
+  }
+
+  check(event,name){
+
+    if(name=="all"){
+      this.category_0=true;
+      this.category_1=false;
+      this.category_2=false; 
+      this.category_3=false;
+    }
+    
+    else if(name=="herbalists"){
+      this.category_0=false;
+      this.category_1=true;
+      this.category_2=false; 
+      this.category_3=false;
+    }
+
+    else if(name=="aromatic"){
+      this.category_0=false;
+      this.category_1=false;
+      this.category_2=true; 
+      this.category_3=false;
+    }
+
+    else if(name=="decoration"){
+      this.category_0=false;
+      this.category_1=false;
+      this.category_2=false; 
+      this.category_3=true;
+    }
+
   }
 
 }
