a3be57d807d5754140293440bb454d7325c92348
NourNiazy <nourmostafa01551@gmail.com>
2020-09-30 19:25:55 +0200

    Nour

--- Angular-Project/src/app/home-page/home-page.component.ts
+++ Angular-Project/src/app/home-page/home-page.component.ts
@@ -1,9 +1,5 @@
-<<<<<<< HEAD
-import { Component, OnInit ,Input} from '@angular/core';
-=======
 import {  Component, OnInit} from '@angular/core';
 import { ProductService } from '../Services/product.service';
->>>>>>> 7b31974e95a335da0e33199ee02990c21ebb0ac6
 
 @Component({
   selector: 'app-home-page',
@@ -12,13 +8,9 @@ import { ProductService } from '../Services/product.service';
 })
 export class HomePageComponent implements OnInit {
   
-<<<<<<< HEAD
-  constructor() { }
-=======
   public Sales=[];
 
   constructor(private GetAllBestSeller:ProductService) { }
->>>>>>> 7b31974e95a335da0e33199ee02990c21ebb0ac6
 
   ngOnInit(): void {
     this.GetAllBestSeller.getAllBestSeller().subscribe(bestSeller=>this.Sales=bestSeller);
